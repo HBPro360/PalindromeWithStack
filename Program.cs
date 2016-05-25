fdcf9bcc5c8dd37cdce84dc4b2e57e79a0b4bbd3
HBPro360 <jphamm360@gmail.com>
2016-05-24 21:28:27 -0500

    PalindromeWithStack

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeWithStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<Char> st = new Stack<Char>();
+            Console.Write("Enter Word:");
+            string word = Console.ReadLine();
+            for(int i = 0; i< word.Length; i++)
+            {
+                st.Push(word[i]);
+            }
+            string newWord = string.Empty;
+            for (int i = 0; i < word.Length; i++)
+            {
+                newWord += st.Pop();
+                //newWord = newWord + st.Pop();
+            }
+            if (word.ToLower() == newWord.ToLower())
+            {
+                Console.WriteLine(newWord + " is a Palindrome.");
+            }
+            else
+            {
+                Console.WriteLine(newWord + " is NOT a Palindrome.");
+            }
+            Console.Write("\n\nPress Enter to continue.....");
+            Console.ReadLine();
+
+        }
+    }
+}
